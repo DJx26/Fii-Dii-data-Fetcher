789ba60412ad2dc0b31450ac209f71b9544517e5
DJx26 <dakshpersonal11@gmail.com>
2025-07-25 21:46:09 +0530

    Update tunnel.py

--- tunnel.py
+++ tunnel.py
@@ -3,6 +3,5 @@ from pyngrok import ngrok
 # Disconnect old tunnels
 for tunnel in ngrok.get_tunnels():
     ngrok.disconnect(tunnel.public_url)
-
 public_url = ngrok.connect(8501)
 print("🌐 App is live at:", public_url)
